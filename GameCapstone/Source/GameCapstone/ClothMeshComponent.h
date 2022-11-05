5239d004aefd61363576d6926f929f7d7de6b5fc
NCTP <cjfgh530@naver.com>
2022-11-05 20:57:14 +0900

    add codes
    Static to Procedural Codes
    Add Additional Attack

---
+++ GameCapstone/Source/GameCapstone/ClothMeshComponent.h
@@ -0,0 +1,116 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ProceduralMeshComponent.h"
+#include "ClothMeshComponent.generated.h"
+
+/**
+ * 
+ */
+
+struct FClothConstraint;
+
+struct FClothParticle
+{
+	FClothParticle()
+		: Position(0, 0, 0)
+		, PrevPosition(0, 0, 0)
+		, Force(0, 0, 0)
+		, Col(255, 255, 255, 255)
+		, ID(-1)
+		, C_idx(-1)
+		, state(1)
+		, conCount(0)
+	{}
+	FVector Position;
+	FVector PrevPosition;
+	FVector Force;
+	FColor Col;
+
+	int32 ID;
+	uint32 C_idx;
+	int8 state, conCount;
+};
+
+UCLASS()
+class GAMECAPSTONE_API UClothMeshComponent : public UProceduralMeshComponent
+{
+	GENERATED_BODY()
+
+public:
+	struct
+	{
+		// SM Deserialized
+		TArray<FVector>            Pos;
+		TArray<FColor>             Col;
+		TArray<FVector>            Normal;
+		TArray<FProcMeshTangent>   Tang;
+		TArray<FVector2D>          UV;
+		TArray<int32>              Ind;
+		TArray<FIntVector>         Tris;
+
+		// Vert Shared Tris
+		TArray<int32>* vtris;
+
+		// SM Buffer Ptrs
+		FPositionVertexBuffer* vb;
+		FStaticMeshVertexBuffer* smvb;
+		FColorVertexBuffer* cvb;
+		FRawStaticIndexBuffer* ib;
+
+		int32 vert_count, ind_count, adj_count, tri_count;
+		bool has_uv, has_col;
+	} m_smData;
+
+	// Mesh Properties
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	UStaticMeshComponent* m_sm;
+
+	UPROPERTY(EditAnywhere, Category = "Cloth Simulation")
+	bool bShowStaticMesh;
+
+	UFUNCTION(BlueprintCallable, CallInEditor, Category = "Cloth Simulation")
+	void StaticToProcedural();
+
+	UClothMeshComponent(const FObjectInitializer& ObjectInitializer);
+
+	void OnRegister() override;
+
+	void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+
+	// --- Cloth Data ---
+	TArray<FClothParticle> Particles;
+	TArray<FClothConstraint> Constraints;
+	TArray<FVector> Normals;
+	TArray<FClothParticle*> VolSamplePts;
+	float restVolume, curVolume, deltaVolume;
+	int32 particleCount;
+
+	// --- State Flags --- 
+	bool clothStateExists, world_collided;
+
+
+};
+struct FClothConstraint
+{
+	FClothConstraint(FClothParticle& Pt_0, FClothParticle& Pt_1, UClothMeshComponent* cloth)
+		: Pt0(Pt_0), Pt1(Pt_1), Cloth(cloth)
+	{
+		// Get Particles Corresponding Vertices Orginal Postions and Rest Length.
+		orgP0 = Cloth->m_smData.Pos[Pt_0.ID]; orgP1 = Cloth->m_smData.Pos[Pt_1.ID];
+		restLength = (orgP1 - orgP0).Size();
+		// ID to Identify Particle/Vertex ID Pair of Constraint. 
+		conID = Pt_0.ID * Pt_1.ID;
+	}
+	FClothConstraint() = delete;
+
+	FClothParticle& Pt0, & Pt1;
+	FVector orgP0, orgP1;
+	float restLength;
+	int32 conID;
+	UClothMeshComponent* Cloth;
+};
---
+++ GameCapstone/Source/GameCapstone/GameCapstone.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GameCapstone.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GameCapstone, "GameCapstone" );
